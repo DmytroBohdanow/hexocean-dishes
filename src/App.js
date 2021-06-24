e68fc6d4e43f6358e2c4fb8b0e2fa88720f64f44
Joker55222 <joker55222@gmail.com>
2021-06-24 02:02:11 +0200

    Project started
    Initialisation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ Component } from "react";
+import { connect } from 'react-redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    render() {
+       console.log(this.props)
+      return (
+      <div className="App">
+        <h1 className="header">Dishes</h1>
+        <div className="header-info">
+          <p>Requruitment exercise for HexOcean</p>
+          <p>Please, fill the form below to make an order</p>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    counter:state.counter
+  }
+}
+
+export default connect(mapStateToProps)(App);
